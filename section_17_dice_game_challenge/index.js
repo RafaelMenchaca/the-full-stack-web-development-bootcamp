ccae3ffc0233a17a2fbe1fa68dd96c3f0a6483d4
Rafael Menchaca <arafael.menchaca@gmail.com>
2025-10-25 12:49:35 -0500

    Section 17 completed, challenge dicee game, random dicee shows a winner, queryselector, setAttribute, DOM, innerHTML

---
+++ section_17_dice_game_challenge/index.js
@@ -0,0 +1,21 @@
+// alert("Hola");
+var randomNumber1 = Math.floor(Math.random() *6) + 1;
+var randomNumber2 = Math.floor(Math.random() *6) + 1;
+
+
+
+document.querySelector(" img.img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png")
+document.querySelector(" img.img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png")
+
+if (randomNumber1 === randomNumber2) {
+    console.log("Draw");
+    document.querySelector("h1").innerHTML = "Draw";
+} else if (randomNumber1 > randomNumber2) {
+    console.log( "Player 1 Left Win");
+    document.querySelector("h1").innerHTML = "Player 1 Left Win";
+} else {
+    console.log("Plater 2 Rigth Win!");
+    document.querySelector("h1").innerHTML = "Player 2 Rigth Win";
+    
+}
+
