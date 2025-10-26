e18f9ff0aedb5a47529e0c8021378a9a2df967fb
Rafael Menchaca <arafael.menchaca@gmail.com>
2025-10-26 11:38:41 -0500

    Section 21, 21, and 23 completed - Intro to unix command line - intro to backend dev - intro to node.js - npm modules, install, dependencies

---
+++ section_21_22_and_23_intro_to_backend_nodejs/2.3 NPM/index.js
@@ -0,0 +1,16 @@
+// var generateName = require('sillyname');
+
+
+import generateName from "sillyname";
+var sillyName = generateName();
+
+
+
+console.log("my name is " + sillyName);
+
+
+import { randomSuperhero } from "superheroes";
+const name = randomSuperhero();
+
+console.log("i am " + name);
+console.log(`i am ${name}`);
