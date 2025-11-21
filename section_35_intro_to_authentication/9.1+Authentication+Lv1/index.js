64026350a2e4230e9b08e6a5b8a4e818b066ca5c
Rafael Menchaca <arafael.menchaca@gmail.com>
2025-11-20 20:51:08 -0600

    Section 35 login and register post route, db created table users and connected to the backend

--- section_35_intro_to_authentication/9.1+Authentication+Lv1/index.js
+++ section_35_intro_to_authentication/9.1+Authentication+Lv1/index.js
@@ -1,5 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "Th3R4f4%123",
+  port: 5432,
+});
+db.connect();
 
 const app = express();
 const port = 3000;
@@ -19,12 +30,35 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
+app.get("/secrets", (req, res) => {
+  res.render("secrets.ejs")
+
+});
+
 app.post("/register", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
   // console.log(email);
   // console.log(password);
 
+  try {
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if(checkResult.rows.length > 0){
+      res.send("Email already exist!");
+    } else {
+      const result = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [
+        email,
+        password
+      ]);
+      console.log(result);
+      res.render("secrets.ejs");
+    }
+  } catch(err) {
+    console.log(err);
+  }
 });
 
 app.post("/login", async (req, res) => {
@@ -32,6 +66,27 @@ app.post("/login", async (req, res) => {
   const password = req.body.password;
   // console.log(mail);
   // console.log(password);
+
+  try {
+  const result = await db.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+  if (result.rows.length > 0) {
+    const user = result.rows[0];
+    const storedPassword = user.password;
+
+    if (password === storedPassword) {
+      res.render("secrets.ejs");
+    } else {
+      res.send("Incorrect Password");
+    }
+  } else {
+    res.send("User not found");
+  }
+} catch (err) {
+  console.log(err);
+}
+
 });
 
 app.listen(port, () => {
