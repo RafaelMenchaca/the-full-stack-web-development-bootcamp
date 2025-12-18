5f6e0ad846d1b561c5feb58eec590b81c811a260
Rafael Menchaca <arafael.menchaca@gmail.com>
2025-12-17 20:00:54 -0600

    Section 36: starting to build react app for practice, ReactDOM.render, html inside the render, root, running the app. JS inside {}

--- section_36_react/279-intro-to-jsx-completed/src/index.jsx
+++ section_36_react/279-intro-to-jsx-completed/src/index.jsx
@@ -1,16 +1,43 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <div>
-    <h1>Hello World!</h1>
-    <p>This is a paragraph.</p>
-  </div>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <div>
+//     <h1>Hello World</h1>
+//     <p>this is a paragraphsssss</p>
+//     <ul>
+//       <li>item 1</li>
+//       <li>item 2</li>
+//       <li>item 3</li>
+//     </ul>
+//   </div>,
+//   document.getElementById("root")
+// );
 
 // If you're running this locally in VS Code use the commands:
 // npm install
 // to install the node modules and
 // npm run dev
 // to launch your react project in your browser
+
+
+// js code inside render
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Rafael";
+const lucky_number = 5;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}</h1>
+    <p>this is a paragraph {lucky_number}</p>
+    <ul>
+      <li>item {3 * 9}</li>
+      <li>item {Math.floor(Math.random() * 10)}</li>
+      <li>item {}</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
--- section_36_react/279-intro-to-jsx/src/index.jsx
+++ section_36_react/279-intro-to-jsx/src/index.jsx
@@ -1,3 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>this is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
 // If you're running this locally in VS Code use the commands:
 // npm install
 // to install the node modules and
